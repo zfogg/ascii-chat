3332634575a6966b3ad7649d7f6acee0a31c0cb4
Craig Palermo <cpalermo2@gmail.com>
2013-11-20 20:00:40 -0500

    added makefiles for client and server

---
+++ client/client.c
@@ -0,0 +1,64 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+
+int main(int argc, char *argv[])
+{
+    int sockfd = 0, n = 0;
+    char recvBuff[1024];
+    struct sockaddr_in serv_addr; 
+
+    // incorrect number of arguments
+    if(argc != 2) {
+        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        return 1;
+    } 
+
+    // try to open a socket
+    memset(recvBuff, '0',sizeof(recvBuff));
+    // error creating socket
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    } 
+
+    // reserve memory space to store IP address
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+    // set type of address to IPV4 and port to 5000
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000); 
+
+    // an error occurred when trying to set server address and port number
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {
+        printf("\n inet_pton error occured\n");
+        return 1;
+    } 
+
+    // failed when trying to connect to the server 
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+       printf("\n Error : Connect Failed \n");
+       return 1;
+    } 
+
+    /* read from the socket as long as the size of the read is > 0 */
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
+        recvBuff[n] = 0;
+        if(fputs(recvBuff, stdout) == EOF) {
+            printf("\n Error : Fputs error\n");
+        }
+    } 
+
+    if(n < 0) {
+        printf("\n Read error \n");
+    } 
+
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,49 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+
+int main(int argc, char *argv[]) {
+    int listenfd = 0, connfd = 0; // set file descriptors for listen and connection
+    struct sockaddr_in serv_addr; // declare struct that will store connection info for socket
+
+    char sendBuff[1025]; // declare buffer for sending data
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0); // initialize socket
+    memset(&serv_addr, '0', sizeof(serv_addr)); // set serv_addr to all 0s
+    memset(sendBuff, '0', sizeof(sendBuff));    // set sendBuff to all 0s
+
+    serv_addr.sin_family = AF_INET; // set address family to IPV4, AF_INET6 would be IPV6
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // set address for socket
+    serv_addr.sin_port = htons(5000); // set port for socket
+
+    // bind socket based on address and ports set in serv_addr
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    // listen on socket listenfd with max backlog of 10 connections
+    listen(listenfd, 10); 
+
+    while(1) {
+        printf("1) Waiting for a connection...\n");
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); // accept a connection
+        printf("2) Connection initiated, sending data.\n");
+        
+        /* repeatedly reset the buffer, fill with new line, then send to socket */
+        int i = 0;
+        while (i < 5) {
+            memset(sendBuff, '0', sizeof(sendBuff));  // reset the buffer with 0s
+            snprintf(sendBuff, sizeof(sendBuff), "The number %d.\n", i);
+            write(connfd, sendBuff, strlen(sendBuff)); // write whatever is in sendBuff to the connection w/ file descriptor connfd
+            i += 1;
+        }
+
+        printf("3) Closing connection.\n---------------------\n");
+        close(connfd);
+    }
+}
