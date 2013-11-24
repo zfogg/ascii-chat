15cf1ca121ec3312bacd853e9c99e3e1e9bc61a1
Zach Fogg <zach.fogg@gmail.com>
2013-11-23 23:49:32 -0500

    Oh right, here are the file deletions.

--- image.c
+++ image.c
@@ -14,7 +14,6 @@
 #include <jpeglib.h>
 
 #include "ascii.h"
-#include "aspect_ratio.h"
 #include "image.h"
 #include "options.h"
 #include "round.h"
@@ -166,7 +165,6 @@ image_t* image_read(FILE *fp) {
     row_stride = jpg.output_width * jpg.output_components;
     buffer = (*jpg.mem->alloc_sarray)((j_common_ptr) &jpg, JPOOL_IMAGE, row_stride, 1);
 
-    aspect_ratio(jpg.output_width, jpg.output_height);
     p = image_new(jpg.output_width, jpg.output_height);
 
     if ( verbose )
