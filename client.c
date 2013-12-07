f593d2a64074d4802199a236cbb9e241af33b595
unscsprt <cpalermo2@gmail.com>
2013-12-07 02:37:42 -0500

    made client try to reconnect after dropping a connection to server

--- client.c
+++ client.c
@@ -10,11 +10,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
 
 #include "ascii.h"
 #include "client.h"
 #include "options.h"
-
 
 int sockfd = 0;
 
@@ -34,48 +34,65 @@ int main(int argc, char *argv[]) {
     char recvBuff[40000];
     struct sockaddr_in serv_addr;
 
-    // Close socket on program exit.
-    if (signal(SIGINT, sig_handler) == SIG_ERR) {
-        printf("Couldn't catch SIGINT\n");
-    }
-
-    // try to open a socket
-    memset(recvBuff, '0',sizeof(recvBuff));
-    // error creating socket
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error: could not create socket \n");
-        return 1;
-    }
-
-    // reserve memory space to store IP address
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
-    // set type of address to IPV4 and port to 5000
-    printf("Connecting on port %d\n", port);
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    // an error occurred when trying to set server address and port number
-    if(inet_pton(AF_INET, address, &serv_addr.sin_addr) <= 0) {
-        printf("\n Error: inet_pton \n");
-        return 1;
-    }
-
-    // failed when trying to connect to the server
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-       printf("\n Error: connect failed \n");
-       return 1;
-    }
+    static const struct timespec
+    ASCII_SLEEP_START = {
+        .tv_sec  = 3,
+        .tv_nsec = 0
+    },
+    ASCII_SLEEP_STOP = {
+        .tv_sec  = 0,
+        .tv_nsec = 0
+    };
 
     /* read from the socket as long as the size of the read is > 0 */
-    ascii_write_init();
-    int read_result = 0;
-    while (0 < (read_result = read(sockfd, recvBuff, sizeof(recvBuff)-1))) {
-        recvBuff[read_result] = 0;
-        ascii_write(recvBuff);
+    while(1) {
+        // Close socket on program exit.
+        if (signal(SIGINT, sig_handler) == SIG_ERR) {
+            printf("Couldn't catch SIGINT\n");
+        }
+
+        // try to open a socket
+        memset(recvBuff, '0',sizeof(recvBuff));
+        // error creating socket
+        if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            printf("\n Error: could not create socket \n");
+            return 1;
+        }
+
+        // reserve memory space to store IP address
+        memset(&serv_addr, '0', sizeof(serv_addr));
+
+        // set type of address to IPV4 and port to 5000
+        printf("Connecting on port %d\n", port);
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port);
+
+        // an error occurred when trying to set server address and port number
+        if(inet_pton(AF_INET, address, &serv_addr.sin_addr) <= 0) {
+            printf("\n Error: inet_pton \n");
+            return 1;
+        }
+
+        int read_result = 0;
+        ascii_write_init();
+
+        printf("Attempting to connect...\n");
+
+        // failed when trying to connect to the server
+        if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            printf("\n Error: connect failed \n");
+        }
+
+        while (0 < (read_result = read(sockfd, recvBuff, sizeof(recvBuff)-1))) {
+            recvBuff[read_result] = 0;
+            ascii_write(recvBuff);
+        }
+
+        ascii_write_destroy();
+
+        nanosleep((struct timespec *)&ASCII_SLEEP_START,(struct timespec *)&ASCII_SLEEP_STOP);
     }
-    ascii_write_destroy();
 
     return 0;
 }
