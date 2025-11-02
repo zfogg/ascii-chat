da069f90a44d83e80576283be2f7d0da96d64cc7
Zachary Fogg <me@zfo.gg>
2025-11-01 21:22:21 -0400

    fix a couple macos issues with types and SAFE_MALLOC usage

--- lib/image2ascii/simd/neon.c
+++ lib/image2ascii/simd/neon.c
@@ -553,8 +553,7 @@ char *render_ascii_image_monochrome_neon(const image_t *image, const char *ascii
   const size_t max_char_bytes = 4; // Max UTF-8 character size
   const size_t len = (size_t)h * ((size_t)w * max_char_bytes + 1);
 
-  char *output;
-  output = len = SAFE_MALLOC(char *);
+  char *output = SAFE_MALLOC(len, char *);
 
   char *pos = output;
   const rgb_pixel_t *pixels = (const rgb_pixel_t *)image->pixels;
