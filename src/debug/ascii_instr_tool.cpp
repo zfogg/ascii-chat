1166db8dadf3464d40db8657e341a7939e9c92c0
Zachary Fogg <me@zfo.gg>
2025-11-09 03:09:59 -0500

    small tweaks to instrumentation code. reentry guard. header includes.

--- src/debug/ascii_instr_tool.cpp
+++ src/debug/ascii_instr_tool.cpp
@@ -1,5 +1,6 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/Attr.h"
+#include "clang/AST/ParentMapContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Lex/Lexer.h"
