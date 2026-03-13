9bf7e1189897fa36049abd1cd1ff5f89d0916c02
Zachary Fogg <me@zfo.gg>
2026-03-12 22:38:36 -0400

    my changes

--- include/ascii-chat/paths.h
+++ include/ascii-chat/paths.h
@@ -27,6 +27,6 @@
  * - Linux/macOS: "${INSTALL_PREFIX}/etc/ascii-chat/config.toml"
  * - Windows: "${INSTALL_PREFIX}\\etc\\ascii-chat\\config.toml"
  */
-#define ASCIICHAT_INSTALL_PREFIX "/usr/local"
+#define ASCIICHAT_INSTALL_PREFIX "/tmp/test-install"
 
 #endif /* ASCIICHAT_PATHS_H */
